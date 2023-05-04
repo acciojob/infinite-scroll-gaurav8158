7858a694121340348dcd69b3e7663c854e8a44ce
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-05-04 23:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 //your code here!
 
+// Define function to load additional content
+function loadMore() {
+  // Make request to server to fetch next set of items
+  // ...
+  
+  // Append items to existing list
+  const list = document.querySelector('#infi-list');
+  for (let i = 1; i <= 20; i++) {
+    const item = document.createElement('li');
+    item.textContent = `Item ${i}`;
+    list.appendChild(item);
+  }
+}
+
+// Add event listener to window object for scroll event
+document.querySelector('#infi-list').addEventListener('scroll', function() {
+ const list = document.querySelector('#infi-list');
+  const listHeight = list.offsetHeight;
+  const scrollPosition =  window.innerHeight + window.pageYOffset;
+  
+  if (scrollPosition > listHeight) {
+    loadMore();
+  }
+});
+
+// Add initial set of items to list
+const list = document.querySelector('#infi-list');
+for (let i = 1; i < 11; i++) {
+  const item = document.createElement('li');
+  item.textContent = `Item ${i}`;
+  list.appendChild(item);
+}
